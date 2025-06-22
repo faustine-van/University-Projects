91ee1c06940d22b8dc0ab208d163f92b3148b1e8
faustine-van <faustinemuhayemariya44@gmail.com>
2025-06-22 23:30:58 +0300

    Remove old Java demo files and unused resources

--- Overriding.java
+++
@@ -1,8 +0,0 @@
-public class Overriding extends Overloading {
-    void show(){
-        System.out.println("hello me");
-    }
-    int area(int a, int b){
-        return a * b; 
-    };
-}
--- X.java
+++
@@ -1,6 +0,0 @@
-public class X {
-    public static void main(String[] args) {
-        int x = 29%6*3-24%7*9/6;
-        System.out.println(x);
-    }
-}
