d3fe6053e49f53df058c791795dedf19953a9596
faustine-van <faustinemuhayemariya44@gmail.com>
2025-06-22 23:25:07 +0300

    mv java files into java-oop-projects folder

---
+++ java-oop-projects/Overriding.java
@@ -0,0 +1,8 @@
+public class Overriding extends Overloading {
+    void show(){
+        System.out.println("hello me");
+    }
+    int area(int a, int b){
+        return a * b; 
+    };
+}
---
+++ java-oop-projects/even_number.java
@@ -0,0 +1,11 @@
+public class even_number {
+    public static void main(String[] args) {
+        int i = 0;
+
+        for (i = 0; i < 100; i++){
+            if(i % 2 == 0){
+                System.out.println(i);
+            }
+        }
+    }
+}
