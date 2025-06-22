9cdc2c6afecd37690fa2a9e3d3ed19b247eea05e
Faustine44 <125466059+faustine-van@users.noreply.github.com>
2025-06-22 23:27:27 +0300

    Delete AddNumbers.java

--- AddNumbers.java
+++
@@ -1,7 +0,0 @@
-public class AddNumbers {
-    public static void main(String[] args) {
-        int a = 12, b = 23, c = 66, res;
-        res = a + b + c;
-        System.out.println("The result is " + res);
-    }
-}
